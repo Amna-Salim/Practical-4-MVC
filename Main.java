6c56fb679628297ea352ef55628f7f8a6f11e5cf
TOSHIBA <TOSHIBA@DESKTOP-767A1C3>
2021-02-14 17:56:26 +0400

    this commit did by Amna in 14/02/2021 at 5:50

--- Main.java
+++ Main.java
@@ -11,8 +11,10 @@ public class Main {
     public static void main(String args[]) {
      
         Model model = new Model();               // Create shared database
-        Controller1 c1 = new Controller1(model); // Create first controller
-        Controller2 c2 = new Controller2(model); // Create second controller
+        Controller1 c1 = new Controller1(model,"Screen1"); // Create first controller
+        Controller2 c2 = new Controller2(model,"Screen2"); // Create second controller
+        Controller2 c3 = new Controller2(model,"Screen3");
+        Controller2 c4 = new Controller2(model,"Screen4");
         
     } // main
   
